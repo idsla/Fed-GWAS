1075cc55547ef2f4b802d932410cfef1347c6f8f
sitaomin1994 <sitaomin@usc.com>
2024-07-10 15:56:30 -0400

    first commit

---
+++ fedgwas/quality_control/qc.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+class QualityControl:
+    def __init__(self):
+        pass
+
+    def check_sex(self, genotype_data: pd.DataFrame, phenotype_data: pd.DataFrame):
+        raise NotImplementedError
+
+    def calculate_missing_rate(self, genotype_data: pd.DataFrame):
+        raise NotImplementedError
+    
+    def calculate_maf(self, genotype_data: pd.DataFrame):
+        raise NotImplementedError
+
+    def hardy_weinberg_test(self, genotype_data: pd.DataFrame):
+        raise NotImplementedError
+
+    def filter_missingness_samples(self, genotype_data: pd.DataFrame, threshold: float) -> pd.DataFrame:    
+        raise NotImplementedError
+
+    def filter_maf_variants(self, genotype_data: pd.DataFrame, threshold: float) -> pd.DataFrame:
+        raise NotImplementedError 
+
+    def filter_data(): 
+        raise NotImplementedError
+    
+    def generate_report(output_path):
+        raise NotImplementedError
