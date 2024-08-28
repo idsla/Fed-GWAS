acd204790895eadc93e7021b96685730f8cfb06f
sitaomin1994 <sitaomin@usc.com>
2024-08-28 08:44:32 -0400

    add parameter file

---
+++ fedgwas/parameters.py
@@ -0,0 +1,19 @@
+
+QUALITY_CONTROL = {
+    "func_name": {
+        'param1': 0.1,     # description of parameters1
+        'param2': 0.2,     # description of parameters2
+    }
+}
+
+KINSHIP = {
+    "final_kinship_estimate": {
+        "firstkin": 0.35,   # threshold for first degree kinship
+        'secondkin': 0.18,  # threshold for second degree kinship
+        'thirdkin': 0.09,   # threshold for third degree kinship
+    }
+}
+
+ASSOCIATION = {
+
+}
